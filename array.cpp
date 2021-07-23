362b05e9c5d240c0b0e52a3caf02b436a8d3eae4
cvdubbs <christophervanwert@gmail.com>
2021-07-22 22:47:31 -0400

    Adding array.cpp and add.c!

---
+++ array.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+
+int main(void)
+{
+    int marks[] = { 96, 92, 78, 54, 86};
+    //printf("%i", marks[4]);
+    for(int i=0;i<5;i++)
+{
+  cout<< marks[i] <<endl;  //for printing the i'th element
+}
+}
